c4f1e07a387bcee9fb4b80d2a2cee42d89534306
Leandro Cáceres <leandrodamian@hotmail.com>
2022-10-18 21:46:05 -0300

    Added animation on login

--- src/app/pages/seller-login/seller-login.component.ts
+++ src/app/pages/seller-login/seller-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
+import { PreloaderService } from '../../services/preloader.service';
 
 @Component({
   selector: 'app-seller-login',
@@ -15,17 +16,21 @@ export class SellerLoginComponent {
 
   constructor(
     private auth: AuthenticationService,
+    private preloader: PreloaderService,
     private router: Router
   ) { }
 
   login() {
     this.errorMessage = '';
+    this.preloader.block();
     this.auth.sellerLogin(this.userName, this.password).subscribe({
       next: (resp: any) => {
+        this.preloader.unblock();
         this.router.navigate(['']);
       },
       error: response => {
-        if (response.status === 403) {
+        this.preloader.unblock();
+        if (response.status === 400) {
           this.errorMessage = 'Usuario y contraseña incorrectos';
         } else {
           this.errorMessage = 'Se produjo un error al iniciar sesión';
